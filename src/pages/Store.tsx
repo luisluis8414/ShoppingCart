80562a270da83fddf6baeaae2ce65c97f61a9fb1
luisluis8414 <luis.wehrberger2@gmail.com>
2023-03-15 10:42:11 +0100

    CleanUp commit

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -7,7 +7,7 @@ export function Store(){
     return (
     <>
     <h1>Store</h1> 
-    <Row xs={1} md={2} lg={3} className="g-3">
+    <Row xs={1} md={2} lg={3} className="g-3 ">
         {storeItems.map(item =>(
             <Col key={item.id}><StoreItem {...item} /></Col>
         ))}
